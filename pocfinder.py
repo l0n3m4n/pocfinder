a205f89275b2f4dfaa077a9396c6ecc2e18a509b
Sh4d0w <143868630+sh4d0wman@users.noreply.github.com>
2024-07-04 11:18:49 +0800

    Fixing error's

--- pocfinder.py
+++ pocfinder.py
@@ -3,23 +3,25 @@ import requests
 import json
 import csv
 import os
-import pandas as pd
 from docx import Document
-from bs4 import BeautifulSoup
 from colorama import init, Fore, Style
 from alive_progress import alive_bar
 from tabulate import tabulate
 
+#import pandas as pd
+#from bs4 import BeautifulSoup
+
+
 init(autoreset=True)
 
 def fetch_exploit_info(cve):
-    """fetch exploit information from an API based on CVE. returns a dictionary with description and entries."""
-    
+    """fetch exploit information from an API based on CVE, returns a dictionary with description and entries."""
+
     api_url = f"https://api.exploit.observer/?keyword={cve}"
     try:
-        with alive_bar(1, bar='smooth', spinner='dots_waves') as bar:
+        with alive_bar(1, bar='smooth', spinner='dots_waves', title=Fore.LIGHTGREEN_EX + 'Fetching Data') as bar:
             response = requests.get(api_url)
-            response.raise_for_status()  # Raise an error for bad responses
+            response.raise_for_status() 
             bar()
             return response.json()
     except requests.exceptions.RequestException as e:
@@ -34,9 +36,14 @@ def save_to_json(exploit_info, filename):
     try:
         with open(filename, 'w') as json_file:
             json.dump(exploit_info, json_file, indent=4)
-        print(f"{Fore.GREEN}Data saved to {filename}{Style.RESET_ALL}")
+        
+        total_entries = sum(len(urls) for urls in exploit_info['entries'].values())
+        print(f"{Fore.GREEN}Data saved to {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{filename}{Style.RESET_ALL}\n{Fore.LIGHTGREEN_EX}Total Entries: {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{total_entries}{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}out of{Style.RESET_ALL} {Fore.LIGHTBLUE_EX}20{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}entries display on terminal{Style.RESET_ALL}")
+
+        
     except Exception as e:
         print(f"{Fore.RED}Error saving to {filename}: {e}{Style.RESET_ALL}")
+
 
 def save_to_csv(exploit_info, filename):
     """Save exploit information to a CSV file."""
@@ -44,12 +51,16 @@ def save_to_csv(exploit_info, filename):
         with open(filename, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(['File Type', 'URLs'])
+            total_entries = 0
+            
             for file_type, urls in exploit_info['entries'].items():
                 for url in urls:
                     writer.writerow([file_type, url])
-        print(f"{Fore.GREEN}Data saved to {filename}{Style.RESET_ALL}")
+                    total_entries += 1  #
+        print(f"{Fore.LIGHTGREEN_EX}Data saved to {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{filename}{Style.RESET_ALL}\n{Fore.LIGHTGREEN_EX}Total Entries: {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{total_entries}{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}out of{Style.RESET_ALL} {Fore.LIGHTBLUE_EX}20{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}entries display on terminal{Style.RESET_ALL}")
     except Exception as e:
         print(f"{Fore.RED}Error saving to {filename}: {e}{Style.RESET_ALL}")
+
 
 def save_to_txt(exploit_info, filename):
     """Save exploit information to a TXT file."""
@@ -57,11 +68,14 @@ def save_to_txt(exploit_info, filename):
         with open(filename, 'w') as txt_file:
             txt_file.write(f"Description: {exploit_info['description']}\n")
             txt_file.write("Entries:\n")
+            total_entries = 0
+            
             for file_type, urls in exploit_info['entries'].items():
                 txt_file.write(f"- {file_type}:\n")
                 for url in urls:
                     txt_file.write(f"  - {url}\n")
-        print(f"{Fore.GREEN}Data saved to {filename}{Style.RESET_ALL}")
+                    total_entries += 1 
+        print(f"{Fore.LIGHTGREEN_EX}Data saved to {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{filename}{Style.RESET_ALL}\n{Fore.LIGHTGREEN_EX}Total Entries: {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{total_entries}{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}out of{Style.RESET_ALL} {Fore.LIGHTBLUE_EX}20{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}entries display on terminal{Style.RESET_ALL}")
     except Exception as e:
         print(f"{Fore.RED}Error saving to {filename}: {e}{Style.RESET_ALL}")
 
@@ -72,7 +86,8 @@ def save_to_html(exploit_info, filename):
     try:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         template_path = os.path.join(current_dir, 'templates', 'report.html')
-
+        total_entries = 0  
+        
         with open(template_path, 'r', encoding='utf-8') as template_file:
             template = template_file.read()
 
@@ -82,15 +97,13 @@ def save_to_html(exploit_info, filename):
             for file_type, urls in exploit_info['entries'].items():
                 for url in urls:
                     entries_html += f"<tr><td>{file_type}</td><td><a href='{url}' target='_blank'>{url}</a></td></tr>"
+                    total_entries += 1 
 
             template = template.replace('{{ entries }}', entries_html)
-
+          
         with open(filename, 'w', encoding='utf-8') as html_file:
             html_file.write(template)
-
-        # Print success message
-        print(f"{Fore.GREEN}Data saved to {filename}{Style.RESET_ALL}")
-
+        print(f"{Fore.LIGHTGREEN_EX}Data saved to {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{filename}{Style.RESET_ALL}\n{Fore.LIGHTGREEN_EX}Total Entries: {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{total_entries}{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}out of{Style.RESET_ALL} {Fore.LIGHTBLUE_EX}20{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}entries display on terminal{Style.RESET_ALL}")
     except FileNotFoundError as fnf_error:
         print(f"{Fore.RED}Error: File not found - {fnf_error}{Style.RESET_ALL}")
     except IOError as io_error:
@@ -106,40 +119,37 @@ def save_to_docx(exploit_info, filename):
         doc.add_heading('Description', level=2)
         doc.add_paragraph(exploit_info['description'])
         doc.add_heading('Entries', level=2)
+
+        total_entries = 0
         for file_type, urls in exploit_info['entries'].items():
             doc.add_heading(file_type, level=3)
             for url in urls:
                 doc.add_paragraph(url)
+                total_entries += 1
         doc.save(filename)
-        print(f"{Fore.GREEN}Data saved to {filename}{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTGREEN_EX}Data saved to {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{filename}{Style.RESET_ALL}\n{Fore.LIGHTGREEN_EX}Total Entries: {Style.RESET_ALL}{Fore.LIGHTBLUE_EX}{total_entries}{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}out of{Style.RESET_ALL} {Fore.LIGHTBLUE_EX}20{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}entries display in terminal{Style.RESET_ALL}")
     except Exception as e:
         print(f"{Fore.RED}Error saving to {filename}: {e}{Style.RESET_ALL}")
 
-def save_to_excel(exploit_info, filename):
-    """Save exploit information to an Excel (XLSX) file."""
-    try:
-        excel_data = {'File Type': [], 'URLs': []}
-        for file_type, urls in exploit_info['entries'].items():
-            for url in urls:
-                excel_data['File Type'].append(file_type)
-                excel_data['URLs'].append(url)
-        df = pd.DataFrame(excel_data)
-        df.to_excel(filename, index=False)
-        print(f"{Fore.GREEN}Data saved to {filename}{Style.RESET_ALL}")
-    except Exception as e:
-        print(f"{Fore.RED}Error saving to {filename}: {e}{Style.RESET_ALL}")
 
 
 def display_results_table(exploit_info):
     """Function to display exploit information in a tabular format."""
     try:
         table_data = []
+        count = 0
+        
         for file_type, urls in exploit_info['entries'].items():
             for url in urls:
-                if 'github.com' in url:  # Filter URLs containing github.com
-                    table_data.append([file_type, url])
+                if 'github.com' in url:
+                    table_data.append([Fore.LIGHTCYAN_EX + file_type + Style.RESET_ALL, url])
+                    count += 1
+                    if count >= 20:
+                        break
+            if count >= 20:
+                break
         
-        headers = ['File Type', 'URLs']
+        headers = [Fore.LIGHTBLUE_EX + 'File Type', 'URLs' + Style.RESET_ALL]
         if table_data:
             colored_table = []
             for row in table_data:
@@ -147,25 +157,33 @@ def display_results_table(exploit_info):
                 colored_table.append(colored_row)
             
             table = tabulate(colored_table, headers=headers, tablefmt='grid')
-            print(f"\n{Fore.CYAN}GitHub URLs in Exploit Information:{Style.RESET_ALL}")
-            print(table)
+            print('\n' + table + '\n')
         else:
             print(f"{Fore.YELLOW}No GitHub URLs found in the exploit information.{Style.RESET_ALL}")
+            
     except Exception as e:
         print(f"{Fore.RED}Error displaying results: {e}{Style.RESET_ALL}")
 
-
 def main():
-    banner = """ """
-    parser = argparse.ArgumentParser(description='CVE Exploit Information Finder',
-                                     epilog="Example usage: python3 pocfinder.py  CVE-2024-XXXX --export html --save CVE-2024-XXXX.html")
+    banner = f"""{Fore.LIGHTGREEN_EX}
+██████╗  ██████╗  ██████╗███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔═══██╗██╔════╝██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+██████╔╝██║   ██║██║     █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔═══╝ ██║   ██║██║     ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║     ╚██████╔╝╚██████╗██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝      ╚═════╝  ╚═════╝╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+    Author: l0n3m4n | CVE Details and PoC Finder | Version: v1.2     
+{Style.RESET_ALL}"""
+    parser = argparse.ArgumentParser(description='PoC Finder and CVE details',
+                                     epilog=f"{Fore.LIGHTGREEN_EX}Example usage: python3 pocfinder.py  CVE-2024-XXXX --export html --save CVE-2024-XXXX.html{Style.RESET_ALL}")
+    print(banner)
     parser.add_argument('cve', type=str, help='CVE keyword to search for')
-    parser.add_argument('--export', choices=['json', 'csv', 'txt', 'html', 'docx', 'excel'], default='html',
+    parser.add_argument('--export', choices=['json', 'csv', 'txt', 'html', 'docx'], default='html',
                         help='Export format (default: json)')
     parser.add_argument('--save', type=str, required=True, help='Filename to save the exported data')
     args = parser.parse_args()
 
-    print(f"{Fore.CYAN}Fetching exploit information for CVE-ID {args.cve}...{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}Fetching CVE information for CVE ID: {args.cve}...{Style.RESET_ALL}\n")
     exploit_info = fetch_exploit_info(args.cve)
 
     if exploit_info:
@@ -180,8 +198,7 @@ def main():
             save_to_html(exploit_info, args.save)
         elif args.export == 'docx':
             save_to_docx(exploit_info, args.save)
-        elif args.export == 'excel':
-            save_to_excel(exploit_info, args.save)
+       
 
 if __name__ == "__main__":
     main()
